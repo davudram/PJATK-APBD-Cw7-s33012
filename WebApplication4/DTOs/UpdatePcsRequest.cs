26b249308a76d34217b8e4e0c69b603a2b7c90b7
Davud Ramazanov <davudramazanov2007@gmail.com>
2026-05-17 20:49:42 +0200

    Add annotations in dtos request

--- WebApplication4/DTOs/UpdatePcsRequest.cs
+++ WebApplication4/DTOs/UpdatePcsRequest.cs
@@ -1,7 +1,9 @@
-﻿namespace WebApplication4.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.DTOs;
 
 public record UpdatePcsRequest(
-    string Name,
+    [MaxLength(50)] string Name,
     float Weight,
     int Warranty,
     DateTime CreatedAt,
